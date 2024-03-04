3ae846ca6ab556156c1fe42a75236c6c484f2312
marwan <amsmdm@gmail.com>
2024-03-04 12:56:54 +0200

    date validator

--- validators/data.go
+++ validators/data.go
@@ -10,9 +10,9 @@ import (
 type Date struct {
 	Field     string
 	BeforeNow bool
-	Before    int64 // unix
+	Before    time.Time
 	AfterNow  bool
-	After     int64 // unix
+	After     time.Time
 }
 
 func (d *Date) GetField() string {
@@ -20,7 +20,7 @@ func (d *Date) GetField() string {
 }
 
 func (d *Date) Validate(v any, path []any, lang string) (string, error) {
-	var date, ok = v.(string)
+	var dateStr, ok = v.(string)
 
 	if !ok {
 		return "", &types.ValidationErr{
@@ -30,18 +30,59 @@ func (d *Date) Validate(v any, path []any, lang string) (string, error) {
 			Message: errors.InvalidDataType("string", v, lang),
 		}
 	}
-	var t, err = time.Parse("2006-01-02", date)
+	var dateStruct, err = time.Parse("2006-01-02", dateStr)
+
+	if d.BeforeNow || d.AfterNow {
+		var now, _ = time.Parse("2006-01-02", time.Now().Format("2006-01-02"))
+
+		if d.AfterNow && !dateStruct.After(now) {
+			return "", &types.ValidationErr{
+				Field:   d.Field,
+				Path:    path,
+				Value:   v,
+				Message: shouldAfterErr(now, dateStr, lang),
+			}
+		}
+
+		if d.BeforeNow && !dateStruct.Before(now) {
+			return "", &types.ValidationErr{
+				Field:   d.Field,
+				Path:    path,
+				Value:   v,
+				Message: shouldBeforeErr(now, dateStr, lang),
+			}
+		}
+	} else {
+		if !d.After.IsZero() && !dateStruct.After(d.After) {
+			return "", &types.ValidationErr{
+				Field:   d.Field,
+				Path:    path,
+				Value:   v,
+				Message: shouldAfterErr(d.After, dateStr, lang),
+			}
+		}
+
+		if !d.Before.IsZero() && !dateStruct.Before(d.Before) {
+			return "", &types.ValidationErr{
+				Field:   d.Field,
+				Path:    path,
+				Value:   v,
+				Message: shouldBeforeErr(d.Before, dateStr, lang),
+			}
+		}
+
+	}
 
 	if err != nil {
 		return "", &types.ValidationErr{
 			Field:   d.Field,
 			Path:    path,
-			Value:   date,
+			Value:   dateStr,
 			Message: invalidDateErr(lang),
 		}
 	}
 
-	return t.Format("2006-01-02"), nil
+	return dateStr, nil
 }
 
 func invalidDateErr(lang string) string {
@@ -49,4 +90,21 @@ func invalidDateErr(lang string) string {
 		return "تاريخ غير صالح"
 	}
 	return "invalid date"
+}
+
+func shouldAfterErr(aft time.Time, input string, lang string) string {
+	var sAft = aft.Format("2006-01-02")
+	if lang == "ar" {
+		return "يجب أن يكون التاريخ بعد " + sAft + " (التاريخ المدخل: " + input + ")"
+	}
+	return "Date should be after " + sAft + " (the entered date: " + input + ")"
+}
+
+func shouldBeforeErr(bef time.Time, input string, lang string) string {
+	var sBef = bef.Format("2006-01-02")
+	if lang == "ar" {
+		return "يجب أن يكون التاريخ قبل: " + sBef + " (التاريخ المدخل: " + input + ")"
+	}
+
+	return "Date should be before " + sBef + " (the entered date: " + input + ")"
 }
